161e613fc1ab51458806621785d450439b659689
BurnsD <Dustinburnsactech@gmail.com>
2021-12-04 00:21:58 -0600

    adding java

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,4 +1,52 @@
 // Assignment code here
+const charAmountRange = document.getElementById('charAmountRange')
+const charAmountNumber = document.getElementById('charAmountNumber')
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
+const form = document.getElementById('passwordForm')
+
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const NUNBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91, 96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
+
+charAmountRange.addEventListener('input', syncCharacterAmount)
+charAmountNumber.addEventListener('input', syncCharacterAmount)
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const charAmount = charAmountNumber.value
+  const includeUppercase = includeUppercaseElement.checked
+  const includeNumbers = includeNumbersElement.checked
+  const includeSymbols = includeSymbolscaseElement.checked
+  const password = generatePassword(charAmount, includeUppercase, includeNumbers, includeSymbols)
+})
+
+function generatePassword(charAmount, includeUppercase, includeNumbers, includeSymbols) {
+  String.fromCharCode(65)
+}
+
+function arrayFromLowToHigh(low, high){
+  const array = []
+  for (let i = low; i <= high; i++){
+    array.push(i)
+  }
+  return array
+}
+
+function syncCharacterAmount(e) {
+  const value =e.target.value
+  charAmountNumber.value = value
+  charAmountRange.value = value
+}
+
 
 
 // Get references to the #generate element
