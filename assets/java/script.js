9f0be6f63d46f54434d5ed01c1c34be5f171a07a
BurnsD <Dustinburnsactech@gmail.com>
2021-12-04 22:14:35 -0600

    arrays using character codes

--- assets/java/script.js
+++ assets/java/script.js
@@ -7,6 +7,21 @@ const includeNumbersElement = document.getElementById('includeNumbers')
 const includeSymbolsElement = document.getElementById('includeSymbols')
 // Form the password is generated in
 const form = document.getElementById('passwordGeneratorForm')
+// To display the password
+const passwordDisplay = document.getElementById('passwordDisplay')
+// Setting the array variables using character codes
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91, 96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
+
+
 
 // Event listener for syncing the range and number inputs
 characterAmountNumber.addEventListener('input', syncCharacterAmount)
@@ -23,6 +38,25 @@ form.addEventListener('submit', e => {
   passwordDisplay.innerText = password
   console.log(password)
 })
+
+// Function for generating the password
+function generatePassword(characterAmount, includeUppercase, 
+  includeNumbers, includeSymbols) {
+  let charCodes = LOWERCASE_CHAR_CODES
+  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+  if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+  }
+
+// function to make arrays
+function arrayFromLowToHigh(low, high) {
+  const array = []
+  // for looping through all allowed variables
+  for (let i = low; i <= high; i++) {
+    array.push(i)
+  }
+  return array
+}
 
 // function used to sync the range and number inputs
 function syncCharacterAmount(e) {
