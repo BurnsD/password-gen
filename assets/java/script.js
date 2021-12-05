959f0cef2d5bc32f4bc760170ae4e43d75ee4511
BurnsD <Dustinburnsactech@gmail.com>
2021-12-04 22:02:40 -0600

    added form eventlistener for submit

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,14 +1,33 @@
+// Character Amount
 const characterAmountRange = document.getElementById('characterAmountRange')
 const characterAmountNumber = document.getElementById('characterAmountNumber')
-
+// checkbox elements Uppercase, Numbers, Symbols
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
+// Form the password is generated in
+const form = document.getElementById('passwordGeneratorForm')
 
 // Event listener for syncing the range and number inputs
 characterAmountNumber.addEventListener('input', syncCharacterAmount)
 characterAmountRange.addEventListener('input', syncCharacterAmount)
+
+// EventListener submiting for the password
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const characterAmount = characterAmountNumber.value
+  const includeUppercase = includeUppercaseElement.checked
+  const includeNumbers = includeNumbersElement.checked
+  const includeSymbols = includeSymbolsElement.checked
+  const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+  passwordDisplay.innerText = password
+  console.log(password)
+})
 
 // function used to sync the range and number inputs
 function syncCharacterAmount(e) {
   const value = e.target.value
   characterAmountNumber.value = value
   characterAmountRange.value = value
+  console.log(characterAmountNumber.value)
 }
